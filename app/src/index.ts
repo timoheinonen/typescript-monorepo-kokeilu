d1e37d27a6c1f7fab911c0314d18a0e0384c1136
Timo Heinonen <timo.heinonen@solita.fi>
2023-02-14 15:28:12 +0200

    Lisätty Construct luokan käyttö myös app puolelle, ei ongelmaa

--- app/src/index.ts
+++ app/src/index.ts
@@ -1,8 +1,14 @@
 import {Hello} from 'shared-library';
-//import {LambdaWithDependencyToAwsCdkLib, LambdaWithoutAnyExternalDependencies} from "shared-library/lambda";
+import {LambdaWithDependencyToAwsCdkLib, LambdaWithoutAnyExternalDependencies} from "shared-library/lambda";
+import {Construct} from "constructs";
 
 new Hello();
 
 const lambdaWithoutDeps = new LambdaWithoutAnyExternalDependencies();
+export class CustomConstruct extends Construct {
+    constructor(scope: Construct, id: string) {
+        super(scope, id);
+    }
+}
 
-const lambda = new LambdaWithDependencyToAwsCdkLib(null, 'asd');
+const lambda = new LambdaWithDependencyToAwsCdkLib(new CustomConstruct(null, 'asdasdas'), 'asd');
